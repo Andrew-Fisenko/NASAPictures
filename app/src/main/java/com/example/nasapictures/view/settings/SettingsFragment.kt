ffb80cec7c95bbbba4c097bbf4ec5e4a6654988a
Andrew_F <de5ertf0x.yandex.ru>
2022-07-03 20:03:28 +0300

    Исправлена ошибка с fitsSystemWindows Начата реализация переключения тем

--- app/src/main/java/com/example/nasapictures/view/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/nasapictures/view/settings/SettingsFragment.kt
@@ -1,15 +1,29 @@
 package com.example.nasapictures.view.settings
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.nasapictures.MainActivity
+import com.example.nasapictures.ThemeAqua
+import com.example.nasapictures.ThemeViolet
+import com.example.nasapictures.ThemeWalkman
 import com.example.nasapictures.databinding.FragmentSettingsBinding
 import com.example.nasapictures.viewmodel.PictureOfTheDayViewModel
+import com.google.android.material.tabs.TabLayout
+
 
 class SettingsFragment : Fragment() {
+
+    private lateinit var parentActivity: MainActivity
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        parentActivity = (context as MainActivity)
+
+    }
 
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
@@ -28,8 +42,35 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        setTabLayoutClicks()
     }
+
+    private fun setTabLayoutClicks() = with(binding) {
+        settingLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> {
+                        parentActivity.setCurrentTheme(ThemeViolet)
+                    }
+                    1 -> {
+                        parentActivity.setCurrentTheme(ThemeAqua)
+                    }
+                    2 -> {
+                        parentActivity.setCurrentTheme(ThemeWalkman)
+                    }
+                }
+                requireActivity().recreate()
+            }
+
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+    }
+
 
     companion object {
         fun newInstance() = SettingsFragment()
