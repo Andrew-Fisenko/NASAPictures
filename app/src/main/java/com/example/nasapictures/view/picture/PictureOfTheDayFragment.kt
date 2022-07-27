9cebce17b0197c6d25d9bd06d66c145faa519567
Andrey Fisenko <af@c-soft.dev>
2022-07-27 16:41:34 +0300

    Добавлена возможность плавно увеличить фото дня по нажатию на картинку

--- app/src/main/java/com/example/nasapictures/view/picture/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/nasapictures/view/picture/PictureOfTheDayFragment.kt
@@ -4,9 +4,18 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.*
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.transition.ChangeBounds
+import androidx.transition.ChangeImageTransform
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
 import coil.load
 import com.example.nasapictures.R
 import com.example.nasapictures.databinding.FragmentPictureOfTheDayBinding
@@ -22,6 +31,7 @@ class PictureOfTheDayFragment(i: Int) : Fragment() {
     private var _binding: FragmentPictureOfTheDayBinding? = null
     private val binding get() = _binding!!
     private var date = i
+    var isFlag = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -61,99 +71,130 @@ class PictureOfTheDayFragment(i: Int) : Fragment() {
             })
         }
 
+
+        binding.imageView.setOnClickListener {
+            isFlag = !isFlag
+
+            val params = it.layoutParams as ConstraintLayout.LayoutParams
+
+            val transitionSet = TransitionSet()
+            val changeImageTransform = ChangeImageTransform()
+            val changeBounds = ChangeBounds()
+            changeBounds.duration = 2000L
+            changeImageTransform.duration = 2000L
+
+            transitionSet.ordering = TransitionSet.ORDERING_TOGETHER
+
+            transitionSet.addTransition(changeBounds)// важен порядок, обязетельно changeImageTransform после changeBounds
+            transitionSet.addTransition(changeImageTransform)
+
+            TransitionManager.beginDelayedTransition(binding.root, transitionSet)
+            if (isFlag) {
+                params.height = LinearLayout.LayoutParams.MATCH_PARENT
+                binding.imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+            } else {
+                params.height = LinearLayout.LayoutParams.WRAP_CONTENT
+                binding.imageView.scaleType = ImageView.ScaleType.CENTER_INSIDE
+            }
+            binding.imageView.layoutParams = params
+        }
+
+
+
 //        (requireActivity() as MainActivity).setSupportActionBar(binding.bottomAppBar)
 //        setHasOptionsMenu(true)
-    }
+}
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.menu_main, menu)
-    }
+override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+    super.onCreateOptionsMenu(menu, inflater)
+    inflater.inflate(R.menu.menu_main, menu)
+}
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.action_favorite -> {}
-            R.id.action_settings -> {
-                requireActivity().supportFragmentManager.beginTransaction().hide(this)
-                    .add(R.id.container, SettingsFragment.newInstance()).addToBackStack("").commit()
-            }
-            android.R.id.home -> {
-                activity?.let {
-                    BottomNavigationDrawerFragment().show(it.supportFragmentManager, "tag")
-                }
-            }
+override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when (item.itemId) {
+        R.id.action_favorite -> {}
+        R.id.action_settings -> {
+            requireActivity().supportFragmentManager.beginTransaction().hide(this)
+                .add(R.id.container, SettingsFragment.newInstance()).addToBackStack("").commit()
         }
-        return super.onOptionsItemSelected(item)
-    }
-
-
-    private fun newDate(i: Int): String {
-        val now = Calendar.getInstance()
-        val year = now.get(Calendar.YEAR)
-        val month = now.get(Calendar.MONTH)
-        val day = now.get(Calendar.DAY_OF_MONTH)
-        return if (day - i > 0) {
-            "${year}-${month + 1}-${day - i}"
-        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
-            "${year}-${month}-${31 - i}"
-        } else if (month == 2) {
-            "${year}-${month}-${29 - i}"
-        } else {
-            "${year}-${month}-${32 - i}"
-        }
-    }
-
-    private fun renderData(appState: AppState) {
-        when (appState) {
-            is AppState.Error -> {
-                binding.loadingLayout.visibility = View.GONE
-                binding.imageView.setImageResource(R.drawable.ic_load_error_vector)
-                val throwable = appState.error
-                Snackbar.make(binding.imageView, "Error $throwable", Snackbar.LENGTH_LONG).show()
-            }
-            is AppState.Loading -> {
-                binding.loadingLayout.visibility = View.VISIBLE
-                binding.imageView.setImageResource(R.drawable.ic_no_photo_vector)
-            }
-            is AppState.Success -> {
-                binding.loadingLayout.visibility = View.GONE
-                if (appState.pictureOfTheDayResponseData.mediaType == "video") {
-                    showAVideoUrl(appState.pictureOfTheDayResponseData.url)
-                } else {
-                    binding.imageView.load(appState.pictureOfTheDayResponseData.url)
-                    Snackbar.make(binding.imageView, "Success", Snackbar.LENGTH_LONG).show()
-
-                }
-
-
+        android.R.id.home -> {
+            activity?.let {
+                BottomNavigationDrawerFragment().show(it.supportFragmentManager, "tag")
             }
         }
     }
+    return super.onOptionsItemSelected(item)
+}
 
-    companion object {
-        fun newInstance(i: Int) = PictureOfTheDayFragment(i)
+
+private fun newDate(i: Int): String {
+    val now = Calendar.getInstance()
+    val year = now.get(Calendar.YEAR)
+    val month = now.get(Calendar.MONTH)
+    val day = now.get(Calendar.DAY_OF_MONTH)
+    return if (day - i > 0) {
+        "${year}-${month + 1}-${day - i}"
+    } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+        "${year}-${month}-${31 - i}"
+    } else if (month == 2) {
+        "${year}-${month}-${29 - i}"
+    } else {
+        "${year}-${month}-${32 - i}"
     }
+}
 
-    private fun showAVideoUrl(videoUrl: String) = with(binding) {
-        with(binding){
-            imageView.visibility = View.GONE
-            videoOfTheDay.visibility = View.VISIBLE
-            imageViewSmall.visibility = View.VISIBLE
-            binding.imageViewSmall.setImageResource(R.drawable.ic_no_photo_vector)
-            videoOfTheDay.text = "Сегодня у нас нет картинки дня, но есть видео дня! " +
-                    "${videoUrl.toString()} \n кликни >ЗДЕСЬ< чтобы открыть в новом окне"
-            videoOfTheDay.setOnClickListener {
-                val i = Intent(Intent.ACTION_VIEW).apply {
-                    data = Uri.parse(videoUrl)
-                }
-                startActivity(i)
+private fun renderData(appState: AppState) {
+    when (appState) {
+        is AppState.Error -> {
+            binding.loadingLayout.visibility = View.GONE
+            binding.imageView.setImageResource(R.drawable.ic_load_error_vector)
+            val throwable = appState.error
+            Snackbar.make(binding.imageView, "Error $throwable", Snackbar.LENGTH_LONG).show()
+        }
+        is AppState.Loading -> {
+            binding.loadingLayout.visibility = View.VISIBLE
+            binding.imageView.setImageResource(R.drawable.ic_no_photo_vector)
+        }
+        is AppState.Success -> {
+            binding.loadingLayout.visibility = View.GONE
+            if (appState.pictureOfTheDayResponseData.mediaType == "video") {
+                showAVideoUrl(appState.pictureOfTheDayResponseData.url)
+            } else {
+                binding.imageView.load(appState.pictureOfTheDayResponseData.url)
+                Snackbar.make(binding.imageView, "Success", Snackbar.LENGTH_LONG).show()
+
             }
-        } }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+
+        }
     }
+}
+
+companion object {
+    fun newInstance(i: Int) = PictureOfTheDayFragment(i)
+}
+
+private fun showAVideoUrl(videoUrl: String) = with(binding) {
+    with(binding) {
+        imageView.visibility = View.GONE
+        videoOfTheDay.visibility = View.VISIBLE
+        imageViewSmall.visibility = View.VISIBLE
+        binding.imageViewSmall.setImageResource(R.drawable.ic_no_photo_vector)
+        videoOfTheDay.text = "We do not have any pictures today, but we have a video! " +
+                "\n Tap HERE to watch"
+        videoOfTheDay.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(videoUrl)
+            }
+            startActivity(i)
+        }
+    }
+}
+
+override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+}
 }
 
 
