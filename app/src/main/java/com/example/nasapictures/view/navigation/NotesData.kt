e7624803dfc4152c52e3261ab92902cadf88f672
Andrey Fisenko <af@c-soft.dev>
2022-08-03 12:45:02 +0300

    Реализовано перемещение элементов Mars + проверка на достижение начала и конца списка

--- app/src/main/java/com/example/nasapictures/view/navigation/NotesData.kt
+++ app/src/main/java/com/example/nasapictures/view/navigation/NotesData.kt
@@ -1,7 +1,11 @@
 package com.example.nasapictures.view.navigation
 
+const val TYPE_EARTH = 1
+const val TYPE_MARS = 2
+const val TYPE_HEADER = 3
 
-const val TYPE_EARTH= 1
-const val TYPE_MARS= 2
-const val TYPE_HEADER= 3
-data class Data(val name: String = "Text", val someDescription: String? = "Description", val type: Int =TYPE_MARS)
+data class Data(
+    val name: String = "Text",
+    val someDescription: String? = "Description",
+    val type: Int = TYPE_MARS
+)
--- app/src/main/java/com/example/nasapictures/view/navigation/RecyclerAdapter.kt
+++ app/src/main/java/com/example/nasapictures/view/navigation/RecyclerAdapter.kt
@@ -1,24 +1,33 @@
 package com.example.nasapictures.view.navigation
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nasapictures.databinding.ActivityRecyclerItemEarthBinding
 import com.example.nasapictures.databinding.ActivityRecyclerItemHeaderBinding
 import com.example.nasapictures.databinding.ActivityRecyclerItemMarsBinding
 import com.example.nasapictures.model.AddItem
 import com.example.nasapictures.model.RemoveItem
+import com.google.android.material.snackbar.Snackbar
 
-class RecyclerAdapter(private var listData: List<Data>, val callbackAddEarth: AddItem, val callbackAddMars: AddItem,val callbackRemove: RemoveItem) :
-    RecyclerView.Adapter<RecyclerAdapter.BaseViewHolder>(){
+class RecyclerAdapter(
+    private var listData: MutableList<Data>,
+    val callbackAddEarth: AddItem,
+    val callbackAddMars: AddItem,
+    val callbackRemove: RemoveItem
+) :
+    RecyclerView.Adapter<RecyclerAdapter.BaseViewHolder>() {
 
-    fun setListDataRemove(listDataNew: List<Data>,position: Int){
+    fun setListDataRemove(listDataNew: MutableList<Data>, position: Int) {
         listData = listDataNew
         notifyItemRemoved(position)
     }
 
-    fun setListDataAdd(listDataNew: List<Data>,position: Int){
+    fun setListDataAdd(listDataNew: MutableList<Data>, position: Int) {
         listData = listDataNew
         notifyItemInserted(position)
     }
@@ -27,7 +36,7 @@ class RecyclerAdapter(private var listData: List<Data>, val callbackAddEarth: Ad
         return listData[position].type
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):  BaseViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
         return when (viewType) {
             TYPE_EARTH -> {
                 val binding =
@@ -47,7 +56,7 @@ class RecyclerAdapter(private var listData: List<Data>, val callbackAddEarth: Ad
         }
     }
 
-    override fun onBindViewHolder(holder:  BaseViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
         holder.bind(listData[position])
     }
 
@@ -57,14 +66,14 @@ class RecyclerAdapter(private var listData: List<Data>, val callbackAddEarth: Ad
 
     class HeaderViewHolder(val binding: ActivityRecyclerItemHeaderBinding) :
         BaseViewHolder(binding.root) {
-        override fun bind(data:Data){
+        override fun bind(data: Data) {
             binding.name.text = data.name
         }
     }
 
     inner class EarthViewHolder(val binding: ActivityRecyclerItemEarthBinding) :
         BaseViewHolder(binding.root) {
-        override fun bind(data:Data){
+        override fun bind(data: Data) {
             binding.name.text = data.name
             binding.addItemImageView.setOnClickListener {
                 callbackAddEarth.add(layoutPosition)
@@ -85,6 +94,30 @@ class RecyclerAdapter(private var listData: List<Data>, val callbackAddEarth: Ad
             binding.removeItemImageView.setOnClickListener {
                 callbackRemove.remove(layoutPosition)
             }
+            binding.moveItemUp.setOnClickListener {
+                if (layoutPosition > 1) {
+                    listData.removeAt(layoutPosition).apply {
+                        listData.add(layoutPosition - 1, this)
+                    }
+                    notifyItemMoved(layoutPosition, layoutPosition - 1)
+                } else {
+                    Snackbar.make(binding.name, "Not available to move!", Snackbar.LENGTH_SHORT)
+                        .show()
+                }
+            }
+            binding.moveItemDown.setOnClickListener {
+                if (layoutPosition < listData.size - 1) {
+                    listData.removeAt(layoutPosition).apply {
+                        listData.add(layoutPosition + 1, this)
+                    }
+                    notifyItemMoved(layoutPosition, layoutPosition + 1)
+                } else {
+                    Snackbar.make(binding.name, "Not available to move!", Snackbar.LENGTH_SHORT)
+                        .show()
+                }
+
+            }
+
         }
     }
 
